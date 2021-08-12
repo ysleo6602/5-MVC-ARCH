70e0f79665bc7a64af9e6852e7c58ee8062233a4
DESKTOP-RLELS97\leo66 <leo66@naver.com>
2021-08-12 11:22:58 +0900

    < Step 15: ~ End > using DataSource of Tomcat(context.xml)

--- 5-MVC_ARCH/src/spms/listeners/ContextLoaderListener.java
+++ 5-MVC_ARCH/src/spms/listeners/ContextLoaderListener.java
@@ -2,27 +2,23 @@ package spms.listeners;
 
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import org.apache.commons.dbcp.BasicDataSource;
+import javax.sql.DataSource;
 
 import spms.dao.MemberDao;
 
 public class ContextLoaderListener implements ServletContextListener {
-  BasicDataSource ds;
   
   @Override
   public void contextInitialized(ServletContextEvent event) {
     try {
       ServletContext sc = event.getServletContext();
       
-      ds = new BasicDataSource();
-      ds.setDriverClassName(sc.getInitParameter("driver"));
-      ds.setUrl(sc.getInitParameter("url"));
-      ds.setUsername(sc.getInitParameter("username"));
-      ds.setPassword(sc.getInitParameter("password"));;
+      InitialContext initialContext = new InitialContext();
+      DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/studydb");
       
       MemberDao memberDao = new MemberDao();
       memberDao.setDataSource(ds);
